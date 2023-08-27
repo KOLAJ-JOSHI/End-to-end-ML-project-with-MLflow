a8038b1b5d41abcf78d60babe8069337f7bca56f
Kolaj Joshi <joshikoloj@gmail.com>
2023-08-27 17:07:52 +0100

    data ingestion has been added

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # End-to-end-ML-project-with-MLflow
+
+## Workflow
+
+1. Update config.yaml
+2. Update schema.yaml
+3. Update params.yaml
+4. Update the entity
+5. Update the configuration manager in src config
+6. Update the components
+7. Update the pipeline
+8. Update main.py
+9. Update app.py
+
--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from mlProject import logger
+from mlProject.pipeline.stage_01_data_ingestion import STAGE_NAME, DataIngestionTrainingPipeline
 
-logger.info("This is custom logging")
+STAGE_NAME = "Data ingestion stage"
+
+try:
+    logger.info(f">>>> Stage {STAGE_NAME} started <<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>> stage {STAGE_NAME} completed <<<< \n\nx=======================x")
+except Exception as e:
+    logger.exception(e)
+    raise e
